8cd22102d2a489056113216f86627f9787f0c289
irvanchow78 <152408977+irvanchow78@users.noreply.github.com>
2026-08-04 21:44:27 +0300

    Add files via upload

---
+++ AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General assembly information is controlled through the following set of attributes.
+// Change these attribute values to modify the information associated with the assembly.
+[assembly: AssemblyTitle("EaReplacerTool")]
+[assembly: AssemblyDescription("CrystalIDEA removal and cleanup utility")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyCompany("OBI HOLDINGS PTE LTD.")]
+[assembly: AssemblyProduct("EaReplacerTool")]
+[assembly: AssemblyCopyright("Copyright 2021- OBI HOLDINGS PTE LTD.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly invisible
+// to COM components. If you need to access a type in this assembly from COM,
+// set the ComVisible attribute on that type to true.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4dc3e060-7582-40d0-a4f8-74ff51cb6518")]
+
+// Assembly version information consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all values or use the default for Build and Revision
+// by using the '*' character:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
